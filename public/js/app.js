ecb8fe1f062a169531fde552408cc7e592fc37c3
sim <simeonbourne@gmail.com>
2016-04-20 17:46:02 -0400

    Add forms

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,22 @@
 // Your JS goes here
 $(function(){
 
+	
+
+
+
 	//Return all beers
 	$("#beers").on("submit",function(e){
 		e.preventDefault();
 		$.getJSON("/search", function(data){
 			console.log(data);
 		})
+	});
+
+	$("#bac").on("submit",function(e){
+		e.preventDefault();
+		var age = $("#age").val();
+		console.log(age);
 	});
 
 });
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
+app.use(express.static('bower_components'));
 
 
 var BreweryDb = require('brewerydb-node');
